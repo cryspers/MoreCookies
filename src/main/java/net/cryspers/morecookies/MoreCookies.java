5ca91c3436f0a580cd0f7c1cd84d912b1151c2fe
BAUHAUS <patrickfromgnomedk@gmail.com>
2020-11-24 14:03:44 +0100

    test

--- src/main/java/net/cryspers/morecookies/MoreCookies.java
+++ src/main/java/net/cryspers/morecookies/MoreCookies.java
@@ -5,8 +5,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
-import org.lwjgl.system.CallbackI;
 
 
 public class MoreCookies implements ModInitializer {
@@ -14,11 +12,8 @@ public class MoreCookies implements ModInitializer {
 
 	public static final Cookie PURPLE_COOKIE = new Cookie(new FabricItemSettings(), 2);
 	public static final Cookie GREEN_COOKIE = new Cookie(new FabricItemSettings(), 3);
-<<<<<<< HEAD
 	public static final Cookie YELLOW_COOKIE = new Cookie(new FabricItemSettings(), -1);
-=======
 	public static final Cookie BLUE_COOKIE = new Cookie(new FabricItemSettings(), 3);
->>>>>>> d6e06eec684f5377e9af97768f5c36eab42b43a2
 
 	@Override
 	public void onInitialize() {
@@ -28,11 +23,8 @@ public class MoreCookies implements ModInitializer {
 	public static void registerItems() {
 		Registry.register(Registry.ITEM, new Identifier(MODID, "purple_cookie"), PURPLE_COOKIE);
 		Registry.register(Registry.ITEM, new Identifier(MODID, "green_cookie"), GREEN_COOKIE);
-<<<<<<< HEAD
 		Registry.register(Registry.ITEM, new Identifier(MODID, "yellow_cookie"), YELLOW_COOKIE);
-=======
 		Registry.register(Registry.ITEM, new Identifier(MODID, "blue_cookie"), BLUE_COOKIE);
->>>>>>> d6e06eec684f5377e9af97768f5c36eab42b43a2
 	}
 
 	public static String createItemModelJson(String id, String type) {
