176696a656e754d4af4b41cac713da2d7a8a6fe2
BAUHAUS <patrickfromgnomedk@gmail.com>
2020-11-24 18:45:33 +0100

    Added Cookie Block

--- src/main/java/net/cryspers/morecookies/MoreCookies.java
+++ src/main/java/net/cryspers/morecookies/MoreCookies.java
@@ -1,12 +1,16 @@
 package net.cryspers.morecookies;
 
+import net.cryspers.morecookies.blocks.CookieBlock;
 import net.cryspers.morecookies.items.Cookie;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
-import org.lwjgl.system.CallbackI;
 
 
 public class MoreCookies implements ModInitializer {
@@ -14,45 +18,31 @@ public class MoreCookies implements ModInitializer {
 
 	public static final Cookie PURPLE_COOKIE = new Cookie(new FabricItemSettings(), 2);
 	public static final Cookie GREEN_COOKIE = new Cookie(new FabricItemSettings(), 3);
-<<<<<<< HEAD
 	public static final Cookie YELLOW_COOKIE = new Cookie(new FabricItemSettings(), -1);
-=======
 	public static final Cookie BLUE_COOKIE = new Cookie(new FabricItemSettings(), 3);
->>>>>>> d6e06eec684f5377e9af97768f5c36eab42b43a2
+
+	public static final CookieBlock PURPLE_COOKIE_BLOCK = new CookieBlock(FabricBlockSettings.of(Material.ORGANIC_PRODUCT));
 
 	@Override
 	public void onInitialize() {
 		registerItems();
+		registerBlocks();
 	}
 
 	public static void registerItems() {
-		Registry.register(Registry.ITEM, new Identifier(MODID, "purple_cookie"), PURPLE_COOKIE);
-		Registry.register(Registry.ITEM, new Identifier(MODID, "green_cookie"), GREEN_COOKIE);
-<<<<<<< HEAD
-		Registry.register(Registry.ITEM, new Identifier(MODID, "yellow_cookie"), YELLOW_COOKIE);
-=======
-		Registry.register(Registry.ITEM, new Identifier(MODID, "blue_cookie"), BLUE_COOKIE);
->>>>>>> d6e06eec684f5377e9af97768f5c36eab42b43a2
+		Registry.register(Registry.ITEM, id("purple_cookie"), PURPLE_COOKIE);
+		Registry.register(Registry.ITEM, id("green_cookie"), GREEN_COOKIE);
+		Registry.register(Registry.ITEM, id("yellow_cookie"), YELLOW_COOKIE);
+		Registry.register(Registry.ITEM, id("blue_cookie"), BLUE_COOKIE);
+
+		Registry.register(Registry.ITEM, id("purple_cookie_block"), new BlockItem(PURPLE_COOKIE_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 
-	public static String createItemModelJson(String id, String type) {
-		if ("generated".equals(type) || "handheld".equals(type)) {
-			//The two types of items. "handheld" is used mostly for tools and the like, while "generated" is used for everything else.
-			return "{\n" +
-					"  \"parent\": \"item/" + type + "\",\n" +
-					"  \"textures\": {\n" +
-					"    \"layer0\": \"morecookies:" + id + "\"\n" +
-					"  }\n" +
-					"}";
-		} else if ("block".equals(type)) {
-			//However, if the item is a block-item, it will have a different model json than the previous two.
-			return "{\n" +
-					"  \"parent\": \"morecookies:" + id + "\"\n"
-					+"}";
-		}
-		else {
-			//If the type is invalid, return an empty json string.
-			return "";
-		}
+	public static void registerBlocks() {
+		Registry.register(Registry.BLOCK, id("purple_cookie_block"), PURPLE_COOKIE_BLOCK);
+	}
+
+	public static Identifier id(String path) {
+		return new Identifier(MODID, path);
 	}
 }
